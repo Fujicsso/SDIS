3b3508ee7b8653f1e023dd079e3da55d1120d92f
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-05-06 17:43:43 +0100

    MessageSender now returns a response message

--- src/main/sdis/peer/MessageSender.java
+++ src/main/sdis/peer/MessageSender.java
@@ -3,21 +3,35 @@ package main.sdis.peer;
 import main.sdis.message.Message;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class MessageSender {
 
-    public void sendMessage(Message message, InetAddress destAddress, int destPort) {
-        try {
-            Socket socket = new Socket(destAddress, destPort);
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            out.writeObject(message);
-            socket.close();
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    /**
+     * Sends a message to the given address and returns a response message
+     * @param message the message to send
+     * @param destAddress the destination address
+     * @param destPort the destination port number
+     * @return a response message
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    public Message sendMessage(Message message, InetAddress destAddress, int destPort) throws IOException, ClassNotFoundException {
+        Socket socket = new Socket(destAddress, destPort);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+        out.writeObject(message);
+
+        Message response = (Message) in.readObject();
+
+        socket.close();
+        out.close();
+        in.close();
+
+        return response;
     }
 }
