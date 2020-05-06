ee3ed6db5b7aa861cf5a64293eeb43717900872a
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-05-06 17:42:55 +0100

    add methods to ChordNode interface

--- src/main/sdis/chord/ChordNode.java
+++ src/main/sdis/chord/ChordNode.java
@@ -5,16 +5,28 @@ import java.net.InetSocketAddress;
 public interface ChordNode {
 
     /**
-     * Returns the address of the node's sucessor by searching its local finger tabke
+     * Returns the address of the node's sucessor by searching its local finger table
      * @return the address of the node's successor
      */
-    InetSocketAddress getSuccessor();
+    InetSocketAddress getSuccessorAddress();
+
+    /**
+     * Returns the key of the node's successor by searching its local finger table
+     * @return the key of the node's sucessor
+     */
+    Key getSuccessorKey();
 
     /**
      * Returns the address of the node's predecessor
      * @return the address of the node's predecessor
      */
     InetSocketAddress getPredecessor();
+
+    /**
+     * Returns the closest preceding node of the given key
+     * @return the address of closest preceding node of the key
+     */
+    InetSocketAddress getClosestPrecedingNode(Key key);
 
     /**
      * Finds the object with the given key, e.g. a file
@@ -33,8 +45,9 @@ public interface ChordNode {
     /**
      * Sets the finger table entry on index idx
      * @param entry the entry by which the entry at index idx is to be replaced
+     * @param idx the index
      */
-    void setFingerTableEntry(FingerTableEntry entry);
+    void setFingerTableEntry(FingerTableEntry entry, int idx);
 
     /**
      * Find the sucessor of a given key
