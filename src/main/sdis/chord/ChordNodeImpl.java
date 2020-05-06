718449906d8a85377a4f9ea5ea5e61209b5a6027
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-05-06 22:01:01 +0100

    placeholder implementation of chord methods

--- src/main/sdis/chord/ChordNodeImpl.java
+++ src/main/sdis/chord/ChordNodeImpl.java
@@ -4,6 +4,13 @@ import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 
+import main.sdis.common.Utils;
+import main.sdis.message.Message;
+import main.sdis.message.MessageHeader;
+import main.sdis.message.MessageType;
+import main.sdis.message.SingleArgumentHeader;
+import main.sdis.peer.MessageSender;
+
 /**
  * Represents a chord node
  */
@@ -14,9 +21,11 @@ public class ChordNodeImpl implements ChordNode {
     protected InetSocketAddress predecessor;
     protected List<FingerTableEntry> fingerTable;
     protected NodeKey nodeKey;
+    protected MessageSender messageSender;
 
     /**
      * Creates a new chord node and joins an empty ring
+     * 
      * @param address The node's InetSocketAddress
      */
     public ChordNodeImpl(InetSocketAddress address) {
@@ -26,10 +35,13 @@ public class ChordNodeImpl implements ChordNode {
 
         fingerTable.set(0, new FingerTableEntry(nodeKey, address));
         predecessor = address;
+
+        messageSender = new MessageSender();
     }
 
     /**
      * Creates a new chord node and joins an existing ring
+     * 
      * @param address The node's InetSocketAddress
      * @param contact The contact (bootstrap) node's InetSocketAddress
      */
@@ -38,6 +50,8 @@ public class ChordNodeImpl implements ChordNode {
         this.contact = contact;
         nodeKey = new NodeKey(address);
         initFingerTable();
+
+        messageSender = new MessageSender();
     }
 
     private void initFingerTable() {
@@ -48,8 +62,13 @@ public class ChordNodeImpl implements ChordNode {
     }
 
     @Override
-    public InetSocketAddress getSuccessor() {
+    public InetSocketAddress getSuccessorAddress() {
         return fingerTable.get(0).getAddress();
+    }
+
+    @Override
+    public Key getSuccessorKey() {
+        return fingerTable.get(0).getKey();
     }
 
     @Override
@@ -65,19 +84,54 @@ public class ChordNodeImpl implements ChordNode {
 
     @Override
     public FingerTableEntry getFingerTableEntry(int idx) {
-        // TODO Auto-generated method stub
-        return null;
+        return fingerTable.get(idx);
     }
 
     @Override
-    public void setFingerTableEntry(FingerTableEntry entry) {
-        // TODO Auto-generated method stub
-
+    public void setFingerTableEntry(FingerTableEntry entry, int idx) {
+        fingerTable.set(idx, entry);
     }
 
     @Override
     public InetSocketAddress findSuccessor(Key key) {
-        // TODO Auto-generated method stub
+        if (Utils.isKeyInInterval(key, this.nodeKey, getSuccessorKey()))
+            return getSuccessorAddress();
+        else {
+            InetSocketAddress closestPrecedingNode = getClosestPrecedingNode(key);
+            Key closestPrecedingKey = getKeyByAddress(closestPrecedingNode);
+
+            SingleArgumentHeader<Key> header = new SingleArgumentHeader<Key>(MessageType.GETSUCC, address,
+                    closestPrecedingKey);
+
+            Message message = new Message(header);
+
+            Message responseMessage = messageSender.sendMessage(message, closestPrecedingNode.getAddress(),
+                    closestPrecedingNode.getPort());
+
+            SingleArgumentHeader<InetSocketAddress> responseHeader = (SingleArgumentHeader<InetSocketAddress>) responseMessage
+                    .getHeader();
+
+            return responseHeader.getArg();
+        }
+    }
+
+    @Override
+    public InetSocketAddress getClosestPrecedingNode(Key key) {
+        for (int i = ChordSettings.M; i > 1; i--) {
+            if (Utils.isKeyInOpenInterval(getFingerTableEntry(i).getKey(), nodeKey, key))
+                return getFingerTableEntry(i).getAddress();
+
+        }
+        return address;
+    }
+
+    @Override
+    public Key getKeyByAddress(InetSocketAddress address) {
+        for (FingerTableEntry entry : fingerTable) {
+            if (entry.getAddress().equals(address))
+                return entry.getKey();
+        }
+
         return null;
     }
 
