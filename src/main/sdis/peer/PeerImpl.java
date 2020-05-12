25c60ef0d91c49f84d3ffded83a4f8cf740a725c
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-05-12 19:14:36 +0100

    implement peer side of BACKUP protocol

--- src/main/sdis/peer/PeerImpl.java
+++ src/main/sdis/peer/PeerImpl.java
@@ -1,20 +1,26 @@
 package main.sdis.peer;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import main.sdis.common.ConnectionFailedException;
+import main.sdis.common.MessageSender;
 import main.sdis.common.NodeImpl;
+import main.sdis.file.FileId;
 import main.sdis.message.ConnectMessage;
 import main.sdis.message.ConnectedMessage;
-import main.sdis.message.Message;
-import main.sdis.message.MessageType;
+import main.sdis.message.PutFileMessage;
 
 public class PeerImpl extends NodeImpl implements Peer {
 
     private int identifier;
     private String accessPoint;
     private InetSocketAddress serverAddress;
+    private MessageSender messageSender;
 
     public PeerImpl(int identifier, String accessPoint, InetSocketAddress address, InetSocketAddress serverAddress)
             throws IOException, ConnectionFailedException {
@@ -22,6 +28,7 @@ public class PeerImpl extends NodeImpl implements Peer {
         this.identifier = identifier;
         this.accessPoint = accessPoint;
         this.serverAddress = serverAddress;
+        messageSender = new MessageSender();
 
         connect(serverAddress);
 
@@ -50,7 +57,19 @@ public class PeerImpl extends NodeImpl implements Peer {
 
     @Override
     public void backupFile(String filePath, int replicationDegree) throws IOException {
-        throw new UnsupportedOperationException("Not implemented yet");
+        Path path = Paths.get(filePath);
+        File file = new File(filePath);
+
+        String owner = Files.getOwner(path).getName();
+        long lastModified = file.lastModified();
+
+        byte[] fileBytes = Files.readAllBytes(path);
+
+        FileId fileId = new FileId(file.getName(), lastModified, owner, fileBytes);
+
+        PutFileMessage message = new PutFileMessage(address, fileId, fileBytes, replicationDegree);
+
+        messageSender.sendMessage(message, serverAddress.getAddress(), serverAddress.getPort());
     }
 
     @Override
